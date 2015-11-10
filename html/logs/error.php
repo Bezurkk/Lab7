ab198ad15480d313ee055c469aa227c8a2fcceee
Bezurkk <golie_owner@yahoo.com>
2015-11-10 17:46:34 -0500

    first commit

---
+++ html/logs/error.php
@@ -0,0 +1,8 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2015-11-10 19:58:42 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority	category	message
+2015-11-10T19:58:42+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2015-11-10T19:58:51+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
